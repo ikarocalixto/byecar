a9016e9220d6c0e09e6a9124531fc094bb6c56cb
ikarocalixto <142228802+ikarocalixto@users.noreply.github.com>
2024-04-05 14:31:20 -0300

    versao final

--- python_scripts/insert_veiculos.py
+++ python_scripts/insert_veiculos.py
@@ -16,9 +16,9 @@ from selenium.common.exceptions import TimeoutException
 config = {
     'user': 'root',
     'password': '',
-    'port': 3306, # Atualize de acordo com sua porta 
+    'port': 3307, # Atualize de acordo com sua porta 
     'host': '127.0.0.1',
-    'database': 'root',
+    'database': 'inovetime',
     'raise_on_warnings': True,
 }
 
